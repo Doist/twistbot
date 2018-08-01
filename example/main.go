278a3e04b96a71d9bada85a2b8904bb07f0af738
Artyom Pervukhin <artyom@evasive.ru>
2018-08-01 17:25:13 +0300

    Add example

---
+++ example/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/artyom/twistbot"
+)
+
+func main() {
+	addr, token := "localhost:8080", ""
+	flag.StringVar(&addr, "addr", addr, "address to listen")
+	flag.StringVar(&token, "token", os.Getenv("TOKEN"), "token to verify incoming messages (TOKEN env)")
+	flag.Parse()
+	if err := run(addr, token); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(addr, token string) error {
+	handler := &twistbot.Handler{
+		Token: token,
+		Usage: "I'm just a bot, I can only greet you back, if you say 'Hello'",
+	}
+	handler.AddRule(greeter)
+	return http.ListenAndServe(addr, handler)
+}
+
+func greeter(ctx context.Context, w http.ResponseWriter, msg twistbot.Message) bool {
+	if !strings.HasPrefix(msg.Text, "Hello") {
+		return false
+	}
+	fmt.Fprintf(w, "Hello to you as well, %s!\n", msg.UserName)
+	return true
+}
